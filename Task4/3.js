fd536423dfb9ee8fb1ccb36101f48b873cabc9ba
Dmitry <sax-ru@mail.ru>
2020-10-18 22:33:33 +0300

    fix 3.js

--- Task4/3.js
+++ Task4/3.js
@@ -1,45 +1,57 @@
 // Организовать такую цепочку прототипов, в которой свойства и методы оптимально распределены по объектам:
 //   Есть следующее расширение объектов:
 //   Человек → Сотрудник → Нынешний сотрудник/ бывший сотрудник
+//
+// const john = {
+//     name: "John",
+//     lastName: "Smith",
+//     position: "Senior engineer",
+//     startDate: "10.10.1990",
+//     endDate: "10.10.2000",
+//     baseSalary: "10000",
+//     salaryCurrency: "$",
+//     location: "Russia",
+//     department: "IT",
+//     phoneNumber: "+1234567890",
+//     eat: function() {},
+//     sleep: function() {},
+//     callFriend: function() {},
+//     writeReport: function() {},
+//     organizeMeeting: function () {},
+//     retire: function () {},
+//     startVacation: function () {}
+// };
 
-const john = {
-    name: "John",
-    lastName: "Smith",
-    position: "Senior engineer",
-    startDate: "10.10.1990",
-    endDate: "10.10.2000",
-    baseSalary: "10000",
-    salaryCurrency: "$",
-    location: "Russia",
-    department: "IT",
-    phoneNumber: "+1234567890",
-    eat: function() {},
-    sleep: function() {},
-    callFriend: function() {},
-    writeReport: function() {},
-    organizeMeeting: function () {},
-    retire: function () {},
-    startVacation: function () {}
-};
-
-const Human = function (name, lastName, phoneNumber) {
+const Human = function (name = 'John', lastName = 'Smith', phoneNumber = '+1234567890') {
     this.name = name;
     this.lastName = lastName;
     this.phoneNumber = phoneNumber;
-    this.eat = function () {};
-    this.sleep = function () {};
-    this.callFriend = function () {};
+    this.eat = function () {
+        console.log('Ем');
+    };
+    this.sleep = function () {
+        console.log('Сплю');
+    };
+    this.callFriend = function () {
+        console.log('Звоню другу');
+    };
 }
 
-const Staff = function (position, baseSalary, salaryCurrency, location, department) {
+const Staff = function (position = 'Senior engineer', baseSalary = '10000', salaryCurrency = '$', location = 'Russia', department = 'IT') {
     this.position = position;
     this.baseSalary = baseSalary;
     this.salaryCurrency = salaryCurrency;
     this.location = location;
     this.department = department;
-    this.writeReport = function () {};
-    this.organizeMeeting= function () {};
-    this.startVacation= function () {};
+    this.writeReport = function () {
+        console.log('Пишу отчет');
+    };
+    this.organizeMeeting= function () {
+        console.log('organizeMeeting');
+    };
+    this.startVacation= function () {
+        console.log('startVacation');
+    };
 }
 
 const StaffOff = function (startDate, endDate) {
@@ -48,5 +60,15 @@ const StaffOff = function (startDate, endDate) {
     this.retire = function () {};
 }
 
+
 Staff.prototype = new Human();
 StaffOff.prototype = new Staff();
+
+
+const firedEmployee = new StaffOff('01.01.2019', '10.08.2020');
+
+
+console.log(firedEmployee.name);
+console.log(firedEmployee.startDate);
+console.log(firedEmployee.position);
+console.log(firedEmployee.eat());
